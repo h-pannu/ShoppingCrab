ee583666b337ca0a2e0210790ec7e5407302b57e
HarpreetPannu <er.harpreetpannu@live.com>
2023-10-15 23:31:49 -0700

    Installed nuget packages for entity framework and setup DBContext

--- ShoppingCrab.MVC/Program.cs
+++ ShoppingCrab.MVC/Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using ShoppingCrab.MVC.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+//Setting DbContext
+var getConnString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(getConnString));
 
 var app = builder.Build();
 
