27ffda11f048bc9d403e33271f53123c21422883
HarpreetPannu <er.harpreetpannu@live.com>
2023-10-16 21:18:42 -0700

    Category Crud operation completed alongwith Toastr notifications.

--- ShoppingCrab.MVC/Controllers/CategoryController.cs
+++ ShoppingCrab.MVC/Controllers/CategoryController.cs
@@ -38,9 +38,85 @@ namespace ShoppingCrab.MVC.Controllers
             {
                 _context.Categories.Add(category);
                 _context.SaveChanges();
+                TempData["success"] = "Category created successfully";
                 return RedirectToAction("Index", "Category");
             }
             return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Category? category = _context.Categories.Find(id);
+            //var category = _context.Categories.FirstOrDefault(c => c.Id == id);
+            //var category = _context.Categories.Where(i=>i.Id==id).FirstOrDefault();
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "Category Name cannot be same as Display Order");
+            }
+            if (category.Name.ToLower() == "Admin")
+            {
+                ModelState.AddModelError("", "Category Name cannot contain Admin keyword");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Update(category);
+                _context.SaveChanges();
+                TempData["success"] = "Category updated successfully";
+                return RedirectToAction("Index", "Category");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Category? category = _context.Categories.Find(id);
+            //var category = _context.Categories.FirstOrDefault(c => c.Id == id);
+            //var category = _context.Categories.Where(i=>i.Id==id).FirstOrDefault();
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Category? category = _context.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            _context.Categories.Remove(category);
+            _context.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
+            return RedirectToAction("Index", "Category");
         }
     }
 }
