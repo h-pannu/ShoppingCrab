151a71fa0a34c53bd3f0051d814eb5fa213daced
HarpreetPannu <er.harpreetpannu@live.com>
2023-10-18 17:10:45 -0700

    Implemented Category Repository from Generic repository and Category Interface ICategoryRepository.

---
+++ ShoppingCrab.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,30 @@
+﻿using ShoppingCrab.DataAccess.Data;
+using ShoppingCrab.DataAccess.Repository.IRepository;
+using ShoppingCrab.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCrab.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public CategoryRepository(ApplicationDbContext applicationDbContext) : base(applicationDbContext)
+        {
+            _context = applicationDbContext;
+        }
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        public void Update(Category category)
+        {
+            _context.Categories.Update(category);
+        }
+    }
+}
---
+++ ShoppingCrab.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using ShoppingCrab.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCrab.DataAccess.Repository.IRepository
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+        void Update(Category category);
+        void Save();
+    }
+}
