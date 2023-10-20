9e1aaad139091df5e6ec7a259211dae5194eb4ee
HarpreetPannu <er.harpreetpannu@live.com>
2023-10-19 20:37:22 -0700

    Create functionality working alongwith product image upload.

--- ShoppingCrab.MVC/Areas/Admin/Controllers/ProductController.cs
+++ ShoppingCrab.MVC/Areas/Admin/Controllers/ProductController.cs
@@ -10,9 +10,12 @@ namespace ShoppingCrab.MVC.Areas.Admin.Controllers
     public class ProductController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
-        public ProductController(IUnitOfWork unitOfWork)
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
         {
             _unitOfWork = unitOfWork;
+            _webHostEnvironment = webHostEnvironment;
+
         }
         public IActionResult Index()
         {
@@ -60,6 +63,20 @@ namespace ShoppingCrab.MVC.Areas.Admin.Controllers
 
             if (ModelState.IsValid)
             {
+                string wwwRootPath = _webHostEnvironment.WebRootPath;
+                if(file != null)
+                {
+                    string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                    string productPath = Path.Combine(wwwRootPath, @"images\product");
+
+                    using(var fileStream = new FileStream(Path.Combine(productPath, fileName), FileMode.Create))
+                    {
+                        file.CopyTo(fileStream);
+                    }
+
+                    productVM.Product.ImageUrl = @"images\product" + fileName;
+                }
+
                 _unitOfWork.ProductRepository.Add(productVM.Product);
                 _unitOfWork.Save();
                 
