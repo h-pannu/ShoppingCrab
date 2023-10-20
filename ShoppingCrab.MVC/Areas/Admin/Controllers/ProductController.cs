825e61580274caa18f242b4759d3f4da7286607b
HarpreetPannu <er.harpreetpannu@live.com>
2023-10-19 21:15:34 -0700

    updated code to reupload image and delete the old image.

--- ShoppingCrab.MVC/Areas/Admin/Controllers/ProductController.cs
+++ ShoppingCrab.MVC/Areas/Admin/Controllers/ProductController.cs
@@ -69,15 +69,33 @@ namespace ShoppingCrab.MVC.Areas.Admin.Controllers
                     string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
                     string productPath = Path.Combine(wwwRootPath, @"images\product");
 
+                    if(!string.IsNullOrEmpty(productVM.Product.ImageUrl))
+                    {
+                        //delete the old image
+                        var oldImageUrl = Path.Combine(wwwRootPath, productVM.Product.ImageUrl.TrimStart('\\'));
+                        if(System.IO.File.Exists(oldImageUrl))
+                        {
+                            System.IO.File.Delete(oldImageUrl);
+                        }
+                    }
+
                     using(var fileStream = new FileStream(Path.Combine(productPath, fileName), FileMode.Create))
                     {
                         file.CopyTo(fileStream);
                     }
 
-                    productVM.Product.ImageUrl = @"images\product" + fileName;
+                    productVM.Product.ImageUrl = @"\images\product\" + fileName;
                 }
 
-                _unitOfWork.ProductRepository.Add(productVM.Product);
+                if(productVM.Product.Id == 0)
+                {
+                    _unitOfWork.ProductRepository.Add(productVM.Product);
+                }
+                else
+                {
+                    _unitOfWork.ProductRepository.Update(productVM.Product);
+                }
+                
                 _unitOfWork.Save();
                 
                 TempData["success"] = "Product created successfully";
